8f3e7a8a95b5fc7c2a679e230f2805ab05f6c030
JP Marcantonio <marcantoniojp@gmail.com>
2023-07-06 10:44:42 -0400

    made changes to eliminate list of lists. added env variable and logging

--- Frontiers_186_list_of_customer_emails.py
+++ Frontiers_186_list_of_customer_emails.py
@@ -11,11 +11,37 @@ Your script should output a csv file with list of email addresses.
 
 from woocommerce import API
 import csv
+import os
+import logging
+
+logging.basicConfig(filename='list_of_cust_emails.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+variable_key = "MYSITE2_API_KEY"
+variable_secret = "MYSITE2_API_SECRET"
+
+def check_env_variables():
+    try:
+        os.environ[variable_key]
+        # print(variable_key)
+    except Exception as e:
+        logging.exception(f"The environment variable name must be set: {e}")
+        print(f"Error has occurred: {e}")
+        raise Exception("The environment variable name must be set")
+
+    try:
+        os.environ[variable_secret]
+    except Exception as e:
+        logging.exception(f"The environment variable name must be set: {e}")
+        print(f"Error has occurred: {e}")
+        raise Exception("The environment variable name must be set")
+
+check_env_variables()
 
 wcapi = API(
     url = "http://localhost:8888/mysite2",
-    consumer_key = "<add consumer key>",
-    consumer_secret = "<add consumer secret>",
+    consumer_key =os.getenv(variable_key),
+    consumer_secret =os.getenv(variable_secret),
     version = "wc/v3"
 )
 
@@ -31,10 +57,19 @@ while True:
         break
 
     for customer in customers:
+        # breakpoint()
         customer_email = customer["email"]
-        email_list.append([customer_email])
+        email_list.append(customer_email)
 
+#option 1: change to email_list.append([customer_email])
+# with open(output_file, 'w', newline= '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["User Email Address:"])
+#     writer.writerows(email_list)
+
+#option 2: writing email_list as a list of strings
 with open(output_file, 'w', newline= '') as f:
     writer = csv.writer(f)
     writer.writerow(["User Email Address:"])
-    writer.writerows(email_list)
+    for item in email_list:
+        writer.writerow(item.split(','))
