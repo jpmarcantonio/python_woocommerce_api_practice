1b3959ef615d45dc3924f2fc64b53474f4c18e63
JP Marcantonio <marcantoniojp@gmail.com>
2024-01-30 09:56:45 -0500

    set credentials as env variables, refactored critical logging error in print statement

--- fc5_update_stock_quantity.py
+++ fc5_update_stock_quantity.py
@@ -6,11 +6,41 @@ of each item to a random number between 0 - 50
 """
 from woocommerce import API
 import random
+import os
+import logging
+
+variable_key = "WOO_KEY"
+variable_secret = "WOO_SECRET"
+url = "URL"
+
+def check_environment_variables():
+    try:
+        os.environ[variable_key]
+    except Exception as e:
+        logging.exception(f"The environment variable name must be set: {e}")
+        print(f"Error has occurred: {e}")
+        raise Exception(f"The environment variable name {e} must be set")
+
+    try:
+        os.environ[variable_secret]
+    except Exception as e:
+        logging.exception(f"The environment variable name must be set: {e}")
+        print(f"Error has occurred: {e}")
+        raise Exception(f"The environment variable name {e} must be set")
+
+    try:
+        os.environ[url]
+    except Exception as e:
+        logging.exception(f"The environment variable name must be set: {e}")
+        print(f"Error has occurred: {e}")
+        raise Exception(f"The environment variable name {e} must be set")
+
+check_environment_variables()
 
 wcapi = API(
-    url="http://dev.bootcamp.store.supersqa.com/",
-    consumer_key="CONSUMER_KEY",
-    consumer_secret="CONSUMER_SECRET",
+    url=os.getenv(url),
+    consumer_key=os.getenv(variable_key),
+    consumer_secret=os.getenv(variable_secret),
     version="wc/v3",
     timeout=60
 )
@@ -18,6 +48,7 @@ wcapi = API(
 page = 1
 product_ids = []
 invalid_product_ids = []
+
 # get all products from the store
 while True:
     r = wcapi.get("products", params={"per_page": 100, 'page': page})
@@ -31,7 +62,8 @@ while True:
     page = page + 1
     if not products:
         break
-# for each product, check product type, add to list for logging, update qty
+
+# for each product check product type, sort to list for logging, make API call to set 'manage_stock' to True and update stock quantity
     for product in products:
         product_id = product["id"]
         stock_quantity = product["stock_quantity"]
@@ -39,7 +71,6 @@ while True:
         product_type = product["type"]
         invalid_product_types = ['external', 'grouped']
 
-    # update valid product ids with a random stock quantity
         if product_type not in invalid_product_types:
             product_ids.append(product_id)
 
@@ -52,10 +83,12 @@ while True:
             status_code = r3.status_code
             response_body = r3.content
             if status_code != 200:
-                raise Exception(f"Expected status code 200. Got status code {status_code}"
+                raise Exception(f"Expected status code 200. Got status code {status_code} "
                                 f"For more information about your error, see: {response_body}")
-            print(f"Product Type: {product_type}, Product ID: {product_id}, Stock Quantity: {stock_quantity}")
+            print(f"Product Type: {product_type}, Product ID: {product_id}, Stock Quantity: {updated_product['stock_quantity']}")
 
         else:
             invalid_product_ids.append(product_id)
-print(f"Did not update stock quantity on product ids: {invalid_product_ids} due to being invalid for stock management.")
+
+print(f"Updated stock quantity on {len(product_ids)} items. \n"
+      f"Did not update stock quantity on product ids: {invalid_product_ids} due to being invalid for stock management.")
